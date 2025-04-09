0b1044e601893c4140e4225f235993a312e4f276
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-09 13:58:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let btn = document.getElementById('incrementBtn');
+let p = document.getElementById('counter');
+btn.addEventListener('click',()=>{
+	alert(p.innerText);
+	let para = Number(p.innerText);
+	para = para + 1;
+	p.innerText = para;
+	
+})
